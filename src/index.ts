925897f349575ae6f4065b235c7a72a9a6200ed9
Hacke Wayne <hacklew@hotmail.com>
2021-07-01 21:29:47 +1200

    fix route

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@ const port = process.env.PORT || 8080;
 app.get('/ping', (req, res) => res.send('pong'));
 
 
-app.get('/photos/:term/:size', jwtCheck, hasPermission('photo:search'), searchForPhotos);
+app.get('/photos/:term', jwtCheck, hasPermission('photo:search'), searchForPhotos);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
